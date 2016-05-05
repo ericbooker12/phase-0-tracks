e3d02074846d7950df8307fba3c2ef9fcab6f9ac
ericbooker12 <ericbooker12@gmail.com>
2016-05-05 13:47:20 -0700

    added some puts statements to view object types of variables

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -10,8 +10,28 @@ def ask_vampire
 
 	puts "Would you like some garlic bread from our cafeteria? y or n"
 	likes_garlic = gets.chomp
+	# likes_garlic = convert_to_boolean(likes_garlic)
 
 	puts "Would you like to be put on our health insurance policy? y or n"
 	needs_insurance = gets.chomp
 
+	# This method converts a yes/no statement into boolean true/false
+	def convert_to_boolean(input)
+		if input == "y" || input == "Y" || input == "yes" || input == "Yes" ||input == "YES"
+			return true
+		else
+			return false
+		end
+	end
+
+	puts "------------------------------------------------------------------------------"
+	puts "vampire_name: #{vampire_name.class}"
+	puts "vampire_age: #{vampire_age.class}"
+	puts "year_born: #{year_born.class}"
+	puts "likes_garlic: #{likes_garlic.class}"
+	puts "needs_insurance: #{needs_insurance.class}"
+
 end
+
+ask_vampire
+
