cb34193cc09be18c6017100db8a027368560ac90
ericbooker12 <ericbooker12@gmail.com>
2016-05-05 13:18:00 -0700

    Added questions to vampires.rb

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,10 @@
 def ask_vampire
+	puts "What is your name."
+	vampire_name = gets.chomp
 
+	puts "How old are you?"
+	vampire_age = gets.chomp.to_f 
+	puts "What year were you born?"
+	puts "Would you like some garlic bread from our cafeteria?"
+	puts " Would you like to be put on our health insurance policy?"
 end
