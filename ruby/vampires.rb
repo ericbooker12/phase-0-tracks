3e7b353b4ce77ad52f7b6fabe5bdf61b764c6ea5
ericbooker12 <ericbooker12@gmail.com>
2016-05-06 16:11:58 -0700

    Added conditional statements to determine if employee is a vampire

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,37 +1,64 @@
 def ask_vampire
+	puts "Enter number of employees."
+	num_of_employees = gets.chomp
+
 	puts "What is your name."
-	# vampire_name = gets.chomp
-	vampire_name = "Eric"
+	# name = gets.chomp
+	name = "Eric"
 
 	puts "How old are you?"
-	# vampire_age = gets.chomp.to_i 
-	vampire_age = 38 
+	# age = gets.chomp.to_i 
+	age = 38
 
 	puts "What year were you born?"
 	# year_born = gets.chomp.to_i
 	year_born = 1977
 
+	got_age_right = does_age_match(year_born, age)
+
 	puts "Would you like some garlic bread from our cafeteria? y or n"
 	# likes_garlic = gets.chomp
 	likes_garlic = "n"
-	likes_garlic = convert_to_boolean(likes_garlic)
+	likes_garlic = convert_to_boolean(likes_garlic) # convert yes/no to boolean
 
 	puts "Would you like to be put on our health insurance policy? y or n"
 	# needs_insurance = gets.chomp
-	needs_insurance = "y"
-	needs_insurance = convert_to_boolean(needs_insurance)
+	needs_insurance = "n"
+	needs_insurance = convert_to_boolean(needs_insurance) # convert yes/no to boolean
+
+	# puts "------------------------------------------------------------------------------"
+	# puts "Object types:"
+	# puts "name: #{name}, object type: #{name.class}"
+	# puts "age: #{age}, object type: #{age.class}"
+	# puts "year_born: #{year_born}, object type: #{year_born.class}"
+	# puts "likes_garlic: #{likes_garlic}, object type: #{likes_garlic.class}"
+	# puts "needs_insurance: #{needs_insurance}, object type: #{needs_insurance.class}"
 
 	puts "------------------------------------------------------------------------------"
-	puts "Object types:"
-	puts "vampire_name: #{vampire_name}, object type: #{vampire_name.class}"
-	puts "vampire_age: #{vampire_age}, object type: #{vampire_age.class}"
-	puts "year_born: #{year_born}, object type: #{year_born.class}"
-	puts "likes_garlic: #{likes_garlic}, object type: #{likes_garlic.class}"
-	puts "needs_insurance: #{needs_insurance}, object type: #{needs_insurance.class}"
 
-	puts "------------------------------------------------------------------------------"
+	puts "Name: #{name}"
+	puts "Age: #{age}"
+	puts "Year born: #{year_born}"
+	puts "Got age right? #{got_age_right}"
+	puts "Likes garlic? #{likes_garlic}"
+	puts "Needs insurance? #{needs_insurance}"
 
+	if name == "Drake Cula" || name == "Tu Fang"
+		puts "*** Definitely a vampire ***"
 
+	elsif !got_age_right && !likes_garlic && !needs_insurance
+		puts "Almost certainly a vampire."
+
+	elsif (got_age_right) && (likes_garlic || needs_insurance) 
+		puts "Probably not a vampire."
+
+	elsif !got_age_right && ( !likes_garlic || !needs_insurance)
+		puts "Probably a vampire."
+		
+	else 
+		puts
+		puts "Results inconclusive."
+	end
 
 end # end of ask_vampire method
 
@@ -44,9 +71,11 @@ def convert_to_boolean(input)
 	end
 end
 
+# This method checks to see if age and birth year match
 def does_age_match(birth_year, age)
 	current_year = 2016
 	years_since_birth = current_year - birth_year
+	# if birthday hasn't occurred yet, (years_since_birth - age) == 1, otherwise it will == 0.
 	if years_since_birth - age == 1 || years_since_birth - age == 0
 		return true
 	else
@@ -55,6 +84,11 @@ def does_age_match(birth_year, age)
 
 end
 
-
-
 ask_vampire
+
+
+
+
+
+
+
