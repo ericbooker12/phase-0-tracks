2e6680e4e860c596cbc7c3b9cf36e2f3d03a45b7
Eric Booker <ericbooker12@gmail.com>
2016-05-07 11:50:37 -0700

    Gps 2.1 (#24)
    * modified tooth brushing pseudocode
    
    * Initial commit, added ask_vampire method to vampires.rb
    
    * Added questions to vampires.rb
    
    * added variables to store user input
    
    * added some puts statements to view object types of variables
    
    * added a method to convert yes/no to true/false
    
    * Made some formatting changes
    
    * Just-in-case commit
    
    * Added method to check age
    
    * Added conditional statements to determine if employee is a vampire
    
    * Added loop and arrays to process multiple employees
    
    * decided not o use arrays
    
    * Added question about allergies
    
    * added and updated css sheet

---
+++ ruby/vampires.rb
@@ -0,0 +1,139 @@
+def ask_vampire
+
+	puts "Enter number of employees."
+	num_of_employees = gets.chomp.to_i
+
+	puts "Number of employees to process: #{num_of_employees}"
+
+	i = 0 	# loop iterator
+	num_of_employees.times do # begin looping through employees and get answers from each one.
+
+		puts "\nEmployee #{i + 1}:"
+
+		puts "What is your name."
+		# name = gets.chomp  
+		name = "Eric Booker"
+
+		puts "How old are you?"
+		# age = gets.chomp.to_i 
+		age = 18.to_i
+
+		puts "What year were you born?"
+		# year_born = gets.chomp.to_i
+		year_born = 1977.to_i
+
+		# check to see if age is correct with does_age_match helper method.
+		got_age_right = does_age_match(year_born, age) 
+
+		puts "Would you like some garlic bread from our cafeteria? y or n"
+		# capture yes/no answer in likes_garlic, convert it to boolean.
+		# likes_garlic = convert_to_boolean(gets.chomp) 
+		likes_garlic = true
+
+		puts "Would you like to be put on our health inurance policy? y or n"
+		# needs_insurance = convert_to_boolean(gets.chomp)
+		needs_insurance = true
+
+		puts "Do you have any allergies? y or n"
+		has_allergies = convert_to_boolean(gets.chomp)
+
+		allergies = [] # Create allergies array to store allerigies in next question.
+		if has_allergies
+			puts "List allergies one at a time. Type \"done\" when finished."
+
+			loop do
+				allergy = gets.chomp
+				if allergy != "done"
+					allergies.push(allergy)
+				else
+					break
+				end
+				
+			end
+			has_allergies = false #set to false so loop doesn't start again.
+		end
+			
+		
+		
+		# Check object types. Comment out when done.
+		# puts "------------------------------------------------------------------------------"
+		# puts "Object types:"
+		# puts "num_of_employees: #{num_of_employees}, object type: #{num_of_employees.class}"
+		# puts "name: #{name}, object type: #{name.class}"
+		# puts "age: #{age}, object type: #{age.class}"
+		# puts "year_born: #{year_born}, object type: #{year_born.class}"
+		# puts "likes_garlic: #{likes_garlic}, object type: #{likes_garlic.class}"
+		# puts "needs_insurance: #{needs_insurance}, object type: #{needs_insurance.class}"
+
+		puts "------------------------------------------------------------------------------"
+		puts "Employee #{i + 1}:"
+		puts "Name: #{name}"
+		puts "Age: #{age}"
+		puts "Year born: #{year_born}"
+		puts "Got age right? #{got_age_right}"
+		puts "Likes garlic? #{likes_garlic}"
+		puts "Needs insurance? #{needs_insurance}"
+		if allergies[0] == nil 
+			puts "No allergies"
+		else
+			puts "Allergies: #{allergies}"
+		end
+
+		if name == "Drake Cula" || name == "Tu Fang"
+			puts "*** Definitely a vampire ***"
+
+		elsif !got_age_right && !likes_garlic && !needs_insurance
+			puts "Almost certainly a vampire."
+
+		elsif (got_age_right) && (likes_garlic || needs_insurance) 
+			puts "Probably not a vampire."
+
+		elsif !got_age_right && ( !likes_garlic || !needs_insurance)
+			puts "Probably a vampire."
+			
+		else 
+			puts "Results inconclusive."
+		end
+
+		puts "\n"
+
+		i += 1
+
+	end # end of question do loop.
+
+end # end of ask_vampire method
+
+
+# Helper methods:
+
+# This method converts a yes/no statement into boolean true/false
+def convert_to_boolean(input)
+	if input == "y" || input == "Y" || input == "yes" || input == "Yes" ||input == "YES"
+		return true
+	else
+		return false
+	end
+end
+
+# This method checks to see if age and birth year match
+def does_age_match(birth_year, age)
+	current_year = 2016
+	years_since_birth = current_year - birth_year
+
+	# if birthday hasn't occurred yet, (years_since_birth - age) == 1, otherwise it will == 0.
+	if years_since_birth - age <= 1 && years_since_birth - age >= 0
+		return true
+	else
+		return false
+	end
+
+end
+
+ask_vampire # Call ask_vampire method.
+
+
+
+
+
+
+
