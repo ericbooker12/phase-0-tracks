91eb0b4e1e43a927b2e8fcf0a54aed5dc2447ae2
ericbooker12 <ericbooker12@gmail.com>
2016-05-06 18:01:22 -0700

    Added loop and arrays to process multiple employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,66 +1,105 @@
 def ask_vampire
+
 	puts "Enter number of employees."
-	num_of_employees = gets.chomp
+	num_of_employees = gets.chomp.to_i
 
-	puts "What is your name."
-	# name = gets.chomp
-	name = "Eric"
+	puts "Number of employees to process: #{num_of_employees}"
 
-	puts "How old are you?"
-	# age = gets.chomp.to_i 
-	age = 38
+	# Create array variables for each question
+	name = []
+	age = []
+	year_born = []
+	got_age_right = []
+	likes_garlic = []
+	needs_insurance = []
 
-	puts "What year were you born?"
-	# year_born = gets.chomp.to_i
-	year_born = 1977
+	i = 0 	# loop iterator
+	num_of_employees.times do # begin looping through employees and get answers from each one.
 
-	got_age_right = does_age_match(year_born, age)
+		puts "\nEmployee #{i + 1}:"
 
-	puts "Would you like some garlic bread from our cafeteria? y or n"
-	# likes_garlic = gets.chomp
-	likes_garlic = "n"
-	likes_garlic = convert_to_boolean(likes_garlic) # convert yes/no to boolean
+		puts "What is your name."
+		name << gets.chomp    # push answer to array
+		# name << "Eric"   		# automatically fill array for testing, comment out when finished.
 
-	puts "Would you like to be put on our health insurance policy? y or n"
-	# needs_insurance = gets.chomp
-	needs_insurance = "n"
-	needs_insurance = convert_to_boolean(needs_insurance) # convert yes/no to boolean
+		puts "How old are you?"
+		age << gets.chomp.to_i 
+		# age << 38.to_i
 
-	# puts "------------------------------------------------------------------------------"
-	# puts "Object types:"
-	# puts "name: #{name}, object type: #{name.class}"
-	# puts "age: #{age}, object type: #{age.class}"
-	# puts "year_born: #{year_born}, object type: #{year_born.class}"
-	# puts "likes_garlic: #{likes_garlic}, object type: #{likes_garlic.class}"
-	# puts "needs_insurance: #{needs_insurance}, object type: #{needs_insurance.class}"
+		puts "What year were you born?"
+		year_born << gets.chomp.to_i
+		# year_born << 1977.to_i
 
-	puts "------------------------------------------------------------------------------"
+		got_age_right << does_age_match(year_born.last, age.last)
 
-	puts "Name: #{name}"
-	puts "Age: #{age}"
-	puts "Year born: #{year_born}"
-	puts "Got age right? #{got_age_right}"
-	puts "Likes garlic? #{likes_garlic}"
-	puts "Needs insurance? #{needs_insurance}"
+		puts "Would you like some garlic bread from our cafeteria? y or n"
+		garlic_answer = gets.chomp
+		# garlic_answer = "n"
 
-	if name == "Drake Cula" || name == "Tu Fang"
-		puts "*** Definitely a vampire ***"
+		# convert yes/no to boolean and push it to array
+		likes_garlic << convert_to_boolean(garlic_answer) 
 
-	elsif !got_age_right && !likes_garlic && !needs_insurance
-		puts "Almost certainly a vampire."
+		puts "Would you like to be put on our health insurance policy? y or n"
+		insurance_answer = gets.chomp
+		# insurance_answer = "n"
 
-	elsif (got_age_right) && (likes_garlic || needs_insurance) 
-		puts "Probably not a vampire."
+		# convert yes/no to boolean and push it to array
+		needs_insurance << convert_to_boolean(insurance_answer) 
 
-	elsif !got_age_right && ( !likes_garlic || !needs_insurance)
-		puts "Probably a vampire."
-		
-	else 
-		puts
-		puts "Results inconclusive."
-	end
+		# Check object types. Comment out when done.
+		# puts "------------------------------------------------------------------------------"
+		# puts "Object types:"
+		# puts "num_of_employees: #{num_of_employees}, object type: #{num_of_employees.class}"
+		# puts "name: #{name[i]}, object type: #{name[i].class}"
+		# puts "age: #{age[i]}, object type: #{age[i].class}"
+		# puts "year_born: #{year_born[i]}, object type: #{year_born[i].class}"
+		# puts "likes_garlic: #{likes_garlic[i]}, object type: #{likes_garlic[i].class}"
+		# puts "needs_insurance: #{needs_insurance[i]}, object type: #{needs_insurance[i].class}"
+
+		i += 1
+
+	end # end of question do loop.
+
+	i = 0	# reset iterator to 0
+
+	num_of_employees.times do # loop through employees again and print answers.
+
+		puts "------------------------------------------------------------------------------"
+		puts "Employee #{i + 1}:"
+		puts "Name: #{name[i]}"
+		puts "Age: #{age[i]}"
+		puts "Year born: #{year_born[i]}"
+		puts "Got age right? #{got_age_right[i]}"
+		puts "Likes garlic? #{likes_garlic[i]}"
+		puts "Needs insurance? #{needs_insurance[i]}"
+
+
+		if name[i] == "Drake Cula" || name[i] == "Tu Fang"
+			puts "*** Definitely a vampire ***"
+
+		elsif !got_age_right[i] && !likes_garlic[i] && !needs_insurance[i]
+			puts "Almost certainly a vampire."
+
+		elsif (got_age_right[i]) && (likes_garlic[i] || needs_insurance[i]) 
+			puts "Probably not a vampire."
+
+		elsif !got_age_right[i] && ( !likes_garlic[i] || !needs_insurance[i])
+			puts "Probably a vampire."
+			
+		else 
+			puts "Results inconclusive."
+		end
+
+		puts "\n"
+
+		i += 1 
+
+	end		# end of 2nd loop.
 
 end # end of ask_vampire method
+
+
+# Helper methods:
 
 # This method converts a yes/no statement into boolean true/false
 def convert_to_boolean(input)
@@ -75,6 +114,7 @@ end
 def does_age_match(birth_year, age)
 	current_year = 2016
 	years_since_birth = current_year - birth_year
+
 	# if birthday hasn't occurred yet, (years_since_birth - age) == 1, otherwise it will == 0.
 	if years_since_birth - age == 1 || years_since_birth - age == 0
 		return true
