ffd7184f9a9cd0770b728be74290de1da6f3e1d2
ericbooker12 <ericbooker12@gmail.com>
2016-05-12 14:44:06 -0700

    replace <???> with appropriate methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,67 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+puts "iNvEsTiGaTiOn".swapcase
+puts "iNvEsTiGaTiOn".swapcase!
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+puts ""
+puts "zom".insert(2, 'o')
+
+o_word = "zom"
+o_pos = o_word.index('o')
+puts "zom".insert(o_pos + 1, o_word[o_pos])
 # => “zoom”
 
-# "enhance".<???>
+puts ""
+puts "\"" + "enhance".center("enhance".length + 8) + "\""  # double quotes to show spaces on both sides
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+puts ""
+puts "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+puts ""
+puts "the usual".concat(" suspects")
+puts "the usual".<<(" suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+puts ""
+puts " suspects".insert(0, "the usual")
+puts " suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+puts ""
+puts "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+puts ""
+puts "The mystery of the missing first letter".delete('T')
+
+# Alternate strategies
+phrase = "The mystery of the missing first letter"
+puts phrase.slice(1, phrase.length - 1)
+puts phrase.delete(phrase[0])
+puts phrase.delete(phrase.slice(0))
+puts phrase[1.. -1]
+
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+puts ""
+puts "Elementary,    my   dear        Watson!".split.join(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+puts ""
+puts "z".bytes
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+# 122 is the ascii code for 'z'
 
-# "How many times does the letter 'a' appear in this string?".<???>
+puts ""
+puts "How many times does the letter 'a' appear in this string?".count('a')
 # => 4
+
+
+
+
+
