f59e91a5d1b60737787c976717a7e387c9f079ed
ericbooker12 <ericbooker12@gmail.com>
2016-05-14 15:42:38 -0700

    Initial commit, add breweries

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,71 @@
+
+=begin
+Design a data structure that lists several California breweries and the beers they brew.
+1. Create a hash called california_breweries.
+2. Add names of breweries to hash as keys.
+3. Populate the keys with names of beers sold by the breweries.
+	- names of beers will be in arrays.
+4. Add a key with the name of the city of the brewery.
+=end
+
+def cal_brews
+
+	# names of breweries :
+	california_breweries = { 
+		'21st Amendment' => [],
+		'Drakes' => [],
+		'Faction' => [],
+		'Third St. Aleworks' => [],
+		'Sierra Nevada' => [],
+		'Russian River' => [],
+		'Lost Coast' => [],
+		'Mount St. Helena' => [],
+		'Gordon Biersh' => [],
+		'Moylans' => [],
+		'Hopmonk' => [], 
+		'Mendicino' => [],
+		'Napa Smith' => [],
+		'Anchor' => [],
+		'Speakeasy' => [],
+		'Firestone Walker' => [],
+		'Highway 1' => [],
+		'Bear Republic' => [],
+		'Lagunitas'v=> [],
+		'St. Florian' => [],
+		'Stumptown' => []
+	}
+
+end # cal_brews
+
+cal_brews
+
+
+
+
+
+
+
+
+
+
+# 21st Amendment - San Francisco
+# Drake's - San Leandro
+# Faction - Alameda
+# Third St. Aleworks - Santa Rosa
+# Sierra Nevada - Chico
+# Russian River - Santa Rosa
+# Lost Coast - Eureka
+# Mount St. Helena - Middletown
+# Gordon Biersh - San Jose
+# Moylans - Novato
+# Hopmonk - Sebastopol
+# Mendicino - Ukiah
+# Napa Smith - Napa
+# Anchor - San Francisco
+# Speakeasy - San Francisco
+# Firestone Walker - San Francisco
+# Highway 1 - Pescadero
+# Bear Republic - Healdsburg
+# Lagunitas - Petaluma
+# St. Florian - Windsor
+# Stumptown - Guernville
