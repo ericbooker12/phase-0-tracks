5fac8a1394aa3a6c3576e963bb3ce3c0c73567ca
ericbooker12 <ericbooker12@gmail.com>
2016-05-13 18:14:51 -0700

    Added user input prompts and created hash table

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -10,16 +10,43 @@
 # 3. Link results (values) to symbol keys.
 # 4. Store key/value pair in a hash table. 
 
-puts "Enter client name:"
-:client = gets.chomp
 
-puts "Enter age of client:"
-:age = gets.chomp.to_i
+def get_client_info
+	# Get data from client.
+	puts "Enter client name:"
+	client = gets.chomp
 
-puts "Enter number of children"
-:num_of_children = gets.chomp.to_i
+	puts "Enter age of client:"
+	age = gets.chomp.to_i
 
-puts "Enter decor theme"
-:decor_theme = gets.chomp
+	puts "Does the client have children? y or no"
+	has_children = gets.chomp
+	has_children = convert_to_bool(has_children)
+
+	if has_children
+		puts "Enter number of children"
+		children = gets.chomp.to_i
+	end
+
+	puts "Enter decor theme or type \"none\"."
+	decor = gets.chomp
+
+	designer_hash = {
+		client_name: client,
+		client_age: age,
+		client_has_children: has_children, 
+		num_of_children: children,
+		decor_theme: decor
+	}
+end
+
+
+def convert_to_bool(yes_or_no)
+	if yes_or_no.downcase == "y" || yes_or_no.downcase == "yes"
+		return true
+	else
+		return false
+	end
+end
 
 
