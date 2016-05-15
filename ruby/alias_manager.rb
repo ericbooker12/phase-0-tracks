1b06e369094b5f42bbf5bf54ef0cc9c8956a0570
ericbooker12 <ericbooker12@gmail.com>
2016-05-15 12:58:05 -0700

    Initial commit, write pseudocode for alias_manager

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,11 @@
+=begin
+Pseudocode for alias_manager
+1. Swap first and last name.
+2. Change each vowel for the next vowel
+	- create variable that holds vowels 'aeiou'
+	- figure out how to find only vowels in a string.
+3. Change each consonant for the next consonant in the alphabet.
+	- create variable that holds the alphabet minus vowels.
+	- figure out how to fins only consonants in a string.
+=end
+
