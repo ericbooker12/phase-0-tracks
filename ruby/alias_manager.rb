9d8fa5fa885664344707265387c8668e6f4ea8fb
ericbooker12 <ericbooker12@gmail.com>
2016-05-15 16:54:51 -0700

    added swap_name method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,6 +6,15 @@ Pseudocode for alias_manager
 	- figure out how to find only vowels in a string.
 3. Change each consonant for the next consonant in the alphabet.
 	- create variable that holds the alphabet minus vowels.
-	- figure out how to fins only consonants in a string.
+	- figure out how to find only consonants in a string.
 =end
 
+def swap_name(name)
+	name_array = name.split(" ")
+	swapped_name = name_array[1] + " " + name_array[0]
+	puts "Original name: #{name}"
+	puts "Swapped name #{swapped_name}"
+end
+
+
+swap_name("Eric Booker")
