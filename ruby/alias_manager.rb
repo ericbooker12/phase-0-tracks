e53faf714339f8295b4ba73b731f106fa5a609bb
Eric Booker <ericbooker12@gmail.com>
2016-05-16 12:25:24 -0700

    Branch alias (#31)
    * added method to increment consonants
    
    * Added driver code
    
    * Combined methods into one method
    
    * Fixed errors
    
    * added user input
    
    * Added data structure, Finished release 2

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -9,45 +9,32 @@ Pseudocode for alias_manager
 	- figure out how to find only consonants in a string.
 =end
 
-def swap_name(name)
-	name_array = name.split(" ")
-	swapped_name = name_array[1] + " " + name_array[0]
-	
-end
+def create_alias(name)
+	original_name = name
+	# p "Original name: #{original_name}"
 
-def get_next_vowel
+	if name.include?(' ') # don't swap names if only one word
+		name_array = name.split(" ")
+		swapped_name = name_array[1] + " " + name_array[0]
+		name = swapped_name # name is now the second iteration of name, first and last are swapped
+	end
+	
+	# p "After name swap: #{name}"
+
 	# vowel key:      
 	# a e i o u A E I O U               
 	# 0 1 2 3 4 5 6 7 8 9
 
 	vowel_list = "aeiouAEIOU"
-<<<<<<< HEAD
-	vowel_list_array = vowel_list.split('')
-	p vowel_list_array
-	name = "Eric Booker uuuuUUUU"
-	name_array = name.split('')
-=======
-	name = "Eric Booker uuuuUUUU"
-	name_array = name.split("")
->>>>>>> bb2867ec7d6eefe693ee5d52723dd5c8931cf3d6
-	puts name
 
 	index = 0
 	# parse through each letter in name_array
-<<<<<<< HEAD
-	name_array.each  do |letter_in_name|
+	name.split('').each  do |letter_in_name| # name string is converted to an array
 		# Check to see if it is in vowel_list
-		if vowel_list_array.include?(letter_in_name) 
-
-			# if letter is in vowel_list then store it in vowel_letter.
-=======
-	name_array.each  do |letter|
-		# Check to see if it is in vowel_list
-		if vowel_list.include?(letter) 
+		if vowel_list.include?(letter_in_name) 
 
 			# if letter is in vowel_list then set it to the next letter in the list.
->>>>>>> bb2867ec7d6eefe693ee5d52723dd5c8931cf3d6
-			vowel_letter = name_array[index]
+			vowel_letter = name[index]
 
 			# get the index of the letter in vowel_list and store it in vowel_index
 			vowel_index = vowel_list.index(vowel_letter)
@@ -69,24 +56,66 @@ def get_next_vowel
 
 			# replace vowels in name with next_letter
 			name[index] = next_letter
-<<<<<<< HEAD
-			
-=======
->>>>>>> bb2867ec7d6eefe693ee5d52723dd5c8931cf3d6
-
 		end
 		index += 1
 	end
-	puts name
-end
-
-def get_next_consonant
+	
+	# p "With vowels replaced: #{name}"
+	
 	# consonant key: 
 	# b c d f g h j k l m n  p  q  r  s  t  v  w  x  y  z 
     # 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 
 	# B  C  D  F  G  H  J  K  L  M  N  P  Q  R  S  T  V  W  X  Y  Z
 	# 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41
+
+	consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
+
+	index = 0 # set index to 0 so that we have the index of the letter in each iteration
+	name.split('').each do |letter| # name string is converted to array
+
+		if consonants.include?(letter)
+			cons_index = consonants.index(name[index])
+
+			# handle edge cases
+			if cons_index == 20
+				next_letter_index = 0
+			elsif cons_index == 41
+				next_letter_index = 21
+
+			# handle the rest of the cases
+			else
+				letter_index = consonants.index(letter)
+				next_letter_index = letter_index + 1 
+			end
+			# next_letter = vowel_list[next_letter_index]
+			name[index] = consonants[next_letter_index]
+		end
+		index += 1
+	end
+	
+	# p "With consonants replaced: #{name}"
+
+	alias_name = name #return alias name
+end 
+
+def process_names
+	names_hash = {} # create a hash to store original and alias names.
+	print "Enter your name: "
+	name = gets.chomp
+	
+	while name != "quit" && name != "exit" 
+		names_hash[name] = create_alias(name) # Store original and alias names in hash
+		# puts "#{name}'s alias is #{create_alias(name)}\n\n"
+		print "Enter another name or type exit: "
+		name = gets.chomp
+	end
+
+	# Print the names from the hash
+	puts "\n" #creat a blank line 
+	names_hash.each {|original_name, alias_name| p "#{alias_name} is also known as #{original_name}"}
 end
 
-get_next_vowel
+process_names
+
+
