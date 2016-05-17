f5aa66dd7ee6425852a970cf14be14214fb524a3
Eric Booker <ericbooker12@gmail.com>
2016-05-17 12:03:34 -0700

    Puppy class (#32)
    * added method to increment consonants
    
    * Added driver code
    
    * Combined methods into one method
    
    * Fixed errors
    
    * added user input
    
    * Added data structure, Finished release 2
    
    * initial commit, 6.1 classes

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+# Puppy class
+# attributes:
+# fur_color
+# type (dalmation, lab, etc)
+# disposition (frisky, lazy, excitable)
+# has spots?
+# size
+# eye color
+# tricks
+
+class Puppy
+
+end
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
+
