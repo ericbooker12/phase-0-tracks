e33e77f6d3b9147acee9c18ddb7afeafa5188d69
ericbooker12 <ericbooker12@gmail.com>
2016-05-19 13:55:24 -0700

    another commit before swithcing branch

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,30 @@
 class Santa
 
+	def initialize(gender, ethnicity)
+		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		puts "Santa is a #{@gender} #{@ethnicity}"
+	end
+
+	def reindeer
+		reindeer_ranking = [
+			"Rudolph",
+			"Dasher", 
+			"Dancer", 
+			"Prancer", 
+			"Vixen", 
+			"Comet", 
+			"Cupid", 
+			"Donner", 
+			"Blitzen"
+		]
+	end
+
+	def santas_age(age = 0)
+		@age = age
+	end
+
 	def speak
 		puts "Ho, ho, ho! Haaaapy holidays!"
 	end
@@ -8,12 +33,9 @@ class Santa
 		puts "That was a good #{cookie}."
 	end
 
-	def initialize
-		puts "Initializing Santa instance..."
-	end
 end
 
-santa = Santa.new
+santa = Santa.new("male", "Canadian")
 
 santa.speak
 santa.eat_milk_and_cookies("oreo")
