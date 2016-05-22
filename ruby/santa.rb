ccffe72843e0fbbb4ad0165f7be52ee64b0f78a2
ericbooker12 <ericbooker12@gmail.com>
2016-05-22 13:31:35 -0700

    figured out the get_mad_at method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -8,8 +8,7 @@ class Santa
 		puts "Santa is #{@gender} #{@ethnicity}"
 	end
 
-	def reindeer
-		reindeer_ranking = [
+	@@reindeer_ranking = [
 			"Rudolph",
 			"Dasher", 
 			"Dancer", 
@@ -20,53 +19,130 @@ class Santa
 			"Donner", 
 			"Blitzen"
 		]
+
+	def reindeer_ranking
+		@@reindeer_ranking
 	end
 
-	def santas_age(age = 0)
-		@age = age
+	def get_mad_at(reindeer_name)
+		puts "Santa is mad at #{reindeer_name}"
+		@@reindeer_ranking.delete(reindeer_name) # delete name from array
+		@@reindeer_ranking.push(reindeer_name)	# add it back to the end, can probably combine these into one line
+
 	end
+
+
+	@@age = 0
 
 	def speak
 		puts "Ho, ho, ho! Haaaapy holidays!"
 	end
 
 	def eat_milk_and_cookies(cookie)
-		puts "That was a good #{cookie}."
+		puts "That was a good #{cookie} cookie."
 	end
 
-end
+	def celibrate_birthday
+		@@age += 1
+	end
+
+	#----------GETERS AND SETTERS---------
+
+	# age getter method
+	def age
+		@@age
+	end
+
+	# ethnicity getter method
+	def ethnicity
+		@ethnicity
+	end
+
+	# gender setter method
+	def gender
+		@gender
+	end
+
+	# gender setter method
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+	
+
+end # end of Santa class
 
 genders = ["agender", "female", "bigender", "male", "gender fluid", "N/A"]
 ethnicities = ["black", "latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature", "N/A"]
+cookies = ["Oreo", "chips ahoy", "hydrox", "Joe-joes", "Snickerdoodle", "Oatmeal", "sugar"]
 santas = []
 
-genders.length.times do |i|
-	santas << Santa.new(genders[i], ethnicities[i])
-end
+santa = Santa.new("male", "Canadian")
+print "Santa says: #{santa.speak}"
+santa.eat_milk_and_cookies("oreo")
+puts "Santas age is #{santa.age}"
+santa.celibrate_birthday
+puts "Santas age is #{santa.age}"
 
-puts "---------------------------------------------"
-
-genders.each do |gender|
-	santas << Santa.new(gender, "japanese")
-end
-
-puts "---------------------------------------------"
-
+birthdays = 60
 count = 0
-ethnicities.each do |ethnicity|
-	santas << Santa.new(genders[count], ethnicity)
+while count < birthdays
+	santa.celibrate_birthday
 	count += 1
 end
 
-puts "---------------------------------------------"
+puts "Santa had #{birthdays} birthdays and is now #{santa.age} years old."
+puts ""
+puts santa.age
+puts santa.ethnicity
+puts santa.gender
 
-count = 0
-length = genders.length
-while count < length
-	santas << Santa.new(genders[count], ethnicities[count])
-	count += 1
-end
+# puts ""
+# santa.get_mad_at("Vixen")
+# puts santa.reindeer_ranking
+# puts "------"
+# santa.get_mad_at("Rudolph")
+# puts santa.reindeer_ranking
+# puts "------"
+# santa.get_mad_at("Comet")
+# puts santa.reindeer_ranking
 
+
+
+
+
+# genders.length.times do |i|
+# 	santas << Santa.new(genders[i], ethnicities[i])
+# end
+
+# puts "---------------------------------------------"
+
+# genders.each do |gender|
+# 	santas << Santa.new(gender, "japanese")
+# end
+
+# puts "---------------------------------------------"
+
+# count = 0
+# ethnicities.each do |ethnicity|
+# 	santas << Santa.new(genders[count], ethnicity)
+# 	count += 1
+# end
+
+# puts "---------------------------------------------"
+
+# count = 0
+# length = genders.length
+# while count < length
+# 	santas << Santa.new(genders[count], ethnicities[count])
+# 	santas[count].speak
+# 	santas[count].eat_milk_and_cookies(cookies[count])
+# 	age = santas[count].santas_age(74 + count)
+# 	puts "Santa is #{age}"
+# 	count += 1
+# 	puts "----------------"
+# end
+
+# puts "---------------------------------------------"
 
 # santas << Santa.new("agender", "black")
 # santas << Santa.new("female", "Latino")
@@ -75,11 +151,6 @@ end
 # santas << Santa.new("female", "prefer not to say")
 # santas << Santa.new("gender fluid", "Mystical creature (unicorn)")
 # santas << Santa.new("N/A", "N/A")
-
-
-# santa = Santa.new("male", "Canadian")
-# santa.speak
-# santa.eat_milk_and_cookies("oreo")
 
 # santa = Santa.new("female", "Mexico")
 # santa.speak
