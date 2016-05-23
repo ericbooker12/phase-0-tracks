2c59284cd7c7d102c0bfa1ae3679d79b4c204559
ericbooker12 <ericbooker12@gmail.com>
2016-05-22 18:18:32 -0700

    initial commit, added empty methods

---
+++ ruby/solo.rb
@@ -0,0 +1,55 @@
+# bicycle class
+
+# attributes:
+# type
+# color
+# wheel size
+
+# methods:
+# go
+# stop
+# pedal faster
+# change gears
+# 	arg: gear(1- 10)
+# inflate tires
+# 	arg: air pressure
+
+class Bicycle
+	def initialize(type)
+		@type = type
+		puts "You initialized a new #{type} bike."
+	end
+
+	def bike_color
+
+	end
+
+	def wheel_size
+		
+	end
+
+	def make_bike_go
+
+	end
+
+	def make_bike_stop
+
+	end
+
+	def go_faster
+	end
+
+	def change_gears(gear)
+
+	end
+
+	def inflate_tires(psi)
+
+	end
+end
+
+
+bike1 = Bicycle.new("mountain")
+
+
+
