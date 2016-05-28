46509b24a680c2550a0234b1f96a1f85dbe08487
Eric Booker <ericbooker12@gmail.com>
2016-05-28 12:24:02 -0700

    refactored explore.js

--- js/explore.js
+++ js/explore.js
@@ -4,34 +4,33 @@
 
 
 function reverse(str) {
-	var reverseWord = "";
 	var newWord = str.split(""); // Convert word into an array for easier iteration.
 
 	for (var i = 0; i < str.length/2; i++) {
-		//store the letter to be swapped in letter variable
+		//store the first (or second, or third... depending on iteration) letter to be swapped in letter variable
 		letter = newWord[i]; 
 		//this line will swap letter at position i with letter at position (length - i)
-		newWord[i] = newWord[newWord.length - (1 + i)]; 
+		newWord[i] = newWord[newWord.length  - (i + 1)]; 
 		newWord[newWord.length - (i + 1)] = letter; //replace last (or second to last, third to last ...) with letter
 	} 
 
-	// convert array back into string
-	for (var i = 0; i < str.length; i++) {
-		reverseWord = reverseWord + newWord[i];
-	} 
+	newWord = newWord.join("");  // convert back to string
 
-	return reverseWord;
-
+	return newWord;
 }
 
 //------------------DRIVER CODE------------------
+
+console.log(reverse("Hello my name is Eric"));
+console.log();
 
 var notAPalindrome = "A man, a plan, a canal, Panama!";
 reversedString = (reverse (notAPalindrome));
 
 if (typeof notAPalindrome == "string") {
-	console.log (notAPalindrome);
-	console.log (reversedString);
+	console.log ("Forwards:\n" + notAPalindrome);
+	console.log();
+	console.log ("Backwards:\n" + reversedString);
 }
 
 
