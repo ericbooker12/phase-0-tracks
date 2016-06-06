03d7fe5a9cba27e8751d5536dd971833cfc5206e
Eric Booker <ericbooker12@gmail.com>
2016-06-06 14:45:19 -0700

    loaded drill data in single table with well_id to index it

--- databases/project/8_5-project.rb
+++ databases/project/8_5-project.rb
@@ -46,8 +46,9 @@ end
 
 
 # Add data to the drill_data table
-def load_drill_data(db, depth, drill_rate, temperature)
-  db.execute("INSERT INTO drill_data (depth, drill_rate, temperature) VALUES (?, ?, ?)", [depth, drill_rate, temperature])
+def load_drill_data(db, depth, drill_rate, temperature, well_id)
+  db.execute("INSERT INTO drill_data (depth, drill_rate, temperature, well_id) VALUES (?, ?, ?, ?)", 
+  	[depth, drill_rate, temperature, well_id])
 end
 
 # Open well_info.txt, read data and load it into wells table in well_data database.
@@ -61,22 +62,29 @@ end
 # Create an array to hold names of data files to be used for database.
 # drill_data_files = ["well_data_1.txt", "well_data_2.txt", "well_data_3.txt"]
 
-# Iterate through array of data files and load into database table.
-file_array = ["well_data_1.csv", "well_data_2.csv", "well_data_3.csv"]
+
 lines = []
 
-def fill_table(db, array)
-	File.open("data/#{array}") do |f|
+def fill_table(db, csv_file, well_id)
+	File.open("data/#{csv_file}") do |f|
 		while record = f.gets
 			depth, drill_rate, temp = record.chomp.split(',')
 			line =  "#{depth} #{drill_rate} #{temp}"
-			load_drill_data(db, depth, drill_rate, temp)
+			load_drill_data(db, depth, drill_rate, temp, well_id)
 			# lines.push(line)
 		end
 	end
 end
 
-fill_table(db, file_array[0])
+# Iterate through array of data files and load into database table.
+file_array = ["well_data_1.csv", "well_data_2.csv", "well_data_3.csv"]
+test_file_array = ["test_1.csv", "test_2.csv", "test_3.csv"]
+
+count = 1  # count keeps track of the well_id foreign key
+test_file_array.each do |i|
+	fill_table(db, i, count)
+	count += 1
+end
 
 
 # lines.each do |line|
