03ccdb1a017ca67a41f0c62200d1447761bfe368
Eric Booker <ericbooker12@gmail.com>
2016-06-06 14:24:30 -0700

    Able to fill tables from csv files. FINALLY

--- databases/project/8_5-project.rb
+++ databases/project/8_5-project.rb
@@ -27,8 +27,9 @@ create_drill_data_table_cmd = <<-SQL
     id INTEGER PRIMARY KEY,
     depth REAL,
     drill_rate REAL,
-    temp_in REAL,
-    temp_out REAL
+    temperature REAL,
+    well_id INT,
+    FOREIGN KEY (well_id) REFERENCES wells(id)
   )
 SQL
 
@@ -38,21 +39,61 @@ db.execute(create_well_table_cmd)
 db.execute(create_drill_data_table_cmd)
 
 # Add data to well table 
-def create_well(db, name, location, depth, active)
-  db.execute("INSERT INTO wells (name, location, depth, active) VALUES (?, ?, ?, ?)", [name, location, depth, active])
+def load_well_data(db, name, location, depth, active)
+  db.execute("INSERT INTO wells (name, location, depth, active) VALUES (?, ?, ?, ?)", 
+  	[name, location, depth, active])
+end
+
+
+# Add data to the drill_data table
+def load_drill_data(db, depth, drill_rate, temperature)
+  db.execute("INSERT INTO drill_data (depth, drill_rate, temperature) VALUES (?, ?, ?)", [depth, drill_rate, temperature])
 end
 
 # Open well_info.txt, read data and load it into wells table in well_data database.
-data_array = [];
 File.open("data/well_info.txt") do |f|
 	f.each do |record|
 		name, location, depth, active = record.chomp.split(',')
-		create_well(db, name, location, depth, active)
+		load_well_data(db, name, location, depth, active)
 	end
 end
 
+# Create an array to hold names of data files to be used for database.
+# drill_data_files = ["well_data_1.txt", "well_data_2.txt", "well_data_3.txt"]
+
+# Iterate through array of data files and load into database table.
+file_array = ["well_data_1.csv", "well_data_2.csv", "well_data_3.csv"]
+lines = []
+
+def fill_table(db, array)
+	File.open("data/#{array}") do |f|
+		while record = f.gets
+			depth, drill_rate, temp = record.chomp.split(',')
+			line =  "#{depth} #{drill_rate} #{temp}"
+			load_drill_data(db, depth, drill_rate, temp)
+			# lines.push(line)
+		end
+	end
+end
+
+fill_table(db, file_array[0])
+
+
+# lines.each do |line|
+# 	puts line
+# end
+
+# p lines
+
+# line = f.readline
+# line2 = f.readline
+
+
+# p "line = #{line}"
+# p "line2 = #{line2}"
+
 well_table = db.execute("SELECT * FROM wells")
-p well_table
+# p well_table
 
 
 
