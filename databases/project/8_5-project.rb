a4b06ebfd6c01245e7b06d4ea324d3b8d639de18
Eric Booker <ericbooker12@gmail.com>
2016-06-06 16:54:06 -0700

    Made the program functional

--- databases/project/8_5-project.rb
+++ databases/project/8_5-project.rb
@@ -1,23 +1,20 @@
-# OPERATION KITTEN EXPLOSION!
-
-# talk about Object Relational Mapping (ORM)
 
 # require gems
 require 'sqlite3'
 require 'faker'
 
+
+
 # create SQLite3 database
 db = SQLite3::Database.new("well_data.db")
-# db.results_as_hash = true
 
-# learn about fancy string delimiters
 
 create_well_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS wells(
     id INTEGER PRIMARY KEY,
     name VARCHAR(255),
     location VARCHAR(255),
-    depth int,
+    depth INT,
     active BOOLEAN
   )
 SQL
@@ -33,37 +30,17 @@ create_drill_data_table_cmd = <<-SQL
   )
 SQL
 
-# create the table (if it's not there already)
-	
-db.execute(create_well_table_cmd)
-db.execute(create_drill_data_table_cmd)
-
 # Add data to well table 
 def load_well_data(db, name, location, depth, active)
   db.execute("INSERT INTO wells (name, location, depth, active) VALUES (?, ?, ?, ?)", 
   	[name, location, depth, active])
 end
 
-
 # Add data to the drill_data table
 def load_drill_data(db, depth, drill_rate, temperature, well_id)
   db.execute("INSERT INTO drill_data (depth, drill_rate, temperature, well_id) VALUES (?, ?, ?, ?)", 
   	[depth, drill_rate, temperature, well_id])
 end
-
-# Open well_info.txt, read data and load it into wells table in well_data database.
-File.open("data/well_info.txt") do |f|
-	f.each do |record|
-		name, location, depth, active = record.chomp.split(',')
-		load_well_data(db, name, location, depth, active)
-	end
-end
-
-# Create an array to hold names of data files to be used for database.
-# drill_data_files = ["well_data_1.txt", "well_data_2.txt", "well_data_3.txt"]
-
-
-lines = []
 
 def fill_table(db, csv_file, well_id)
 	File.open("data/#{csv_file}") do |f|
@@ -76,38 +53,81 @@ def fill_table(db, csv_file, well_id)
 	end
 end
 
-# Iterate through array of data files and load into database table.
+# These line exist so that the same data isn't written over and over to the same tables.
+# Comment these lines out to make data persistent.
+db.execute("DROP TABLE IF EXISTS wells")
+db.execute("DROP TABLE IF EXISTS drill_data")
+
+# create the tables 
+db.execute(create_well_table_cmd)
+db.execute(create_drill_data_table_cmd)
+
+puts "Go have a coke while your data is loading..."
+
+# Open well_info.txt, read data and load it into wells table in well_data database.
+File.open("data/well_info.txt") do |f|
+	f.each do |record|
+		name, location, depth, active = record.chomp.split(',')
+		load_well_data(db, name, location, depth, active)
+	end
+end
+
+# Create an array to hold names of data files to be used for database.
 file_array = ["well_data_1.csv", "well_data_2.csv", "well_data_3.csv"]
 test_file_array = ["test_1.csv", "test_2.csv", "test_3.csv"]
 
+# Iterate through array of data files and load into database table.
 count = 1  # count keeps track of the well_id foreign key
-test_file_array.each do |i|
+file_array.each do |i|
 	fill_table(db, i, count)
 	count += 1
 end
 
-
-# lines.each do |line|
-# 	puts line
-# end
-
-# p lines
-
-# line = f.readline
-# line2 = f.readline
-
-
-# p "line = #{line}"
-# p "line2 = #{line2}"
-
 well_table = db.execute("SELECT * FROM wells")
-# p well_table
 
+flag = true
+while flag do
+	puts "Choose a well to evaluate. (1, 2, or 3) or press 'q' to quit."
+	well_chosen = gets.chomp
+	if well_chosen == 'q'
+		flag = false
+		break
+	end
 
+	well_chosen = well_chosen.to_i
 
-# explore ORM by retrieving data
-# kittens = db.execute("SELECT * FROM kittens")
-# kittens.each do |kitten|
-#  puts "#{kitten['name']} is #{kitten['age']}"
-# end
+	puts "What depth would you like to evaluate? or press 'q' to quit."
+	depth_to_eval = gets.chomp
+	if depth_to_eval == 'q'
+		flag = false
+		break
+	end
+
+	input_1 = depth_to_eval.to_i
+	input_2 = input_1 + 1
+
+	new_table = db.execute("SELECT drill_data.temperature, drill_data.depth, wells.name 
+		FROM wells 
+		JOIN drill_data 
+		ON wells.id = drill_data.well_id 
+		AND wells.id = #{well_chosen}
+		AND drill_data.depth > #{input_1}
+		AND drill_data.depth < #{input_2}
+		ORDER BY drill_data.depth
+		LIMIT 1")
+
+	if new_table == []
+		p "There is no data, choose another depth."
+		depth_to_eval = gets.chomp
+		if depth_to_eval == 'q'
+			flag = false
+			break
+		end
+	end
+
+	print "At #{input_1}ft (actual depth: #{new_table[0][1]}ft), " 
+	p "the temperature of the well named \"#{new_table[0][2]}\" is #{new_table[0][0]} deg fahrenheit."
+
+end
+
 
